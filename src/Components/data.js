8b653b0f59581bc5b4c6a01914c102abca776dd5
Emmanuel <adedayoemman@gmail.com>
2024-07-03 08:13:33 +0100

    Myportfolio

---
+++ src/Components/data.js
@@ -0,0 +1,27 @@
+export const projects = [
+    {
+        title: "Tic-Tac-Toe",
+        subtitle: "React & Tailwind Css",
+        description:
+          "A simple Tic-Tac-Toe game built with React. This project helped me learn the basics of React and state management while creating an interactive game.",
+        image: "./tictactoe.gif",
+        link: "https://github.com/Emmanee/mygame",
+      },
+
+    {
+        title: "Stopwatch App",
+        subtitle: "React & Css",
+        description:
+          "A straightforward Stopwatch app built with React. It tracks time with start, stop, and reset features.",
+        image: "",
+        link: "https://github.com/Emmanee/mygame",
+      },
+      
+]
+
+export const skills = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "React"
+]
